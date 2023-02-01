f191c6cbc7924ce2a69bddefbbea7acf864631ad
StanGozie <chigozie.enyoghasi@decagon.dev>
2023-02-01 08:58:59 +0100

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,24 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Employee> emp = new ArrayList<>();
+        emp.add(new Employee("Gozie", 45000.00, 27, 4 ));
+        emp.add(new Employee("Odinaka", 36000.00, 30, 5));
+        emp.add(new Employee("Kene", 24000.00, 26, 2));
+        emp.add(new Employee("Sylvia", 18000.00, 30,3));
+        emp.add(new Employee("Kene", 45000.00, 29,4));
+
+        Collections.sort(emp, new NameAgeComparator());
+
+
+
+        System.out.println(emp);
     }
+
 }
--- src/main/java/org/example/NameAgeComparator.java
+++ src/main/java/org/example/NameAgeComparator.java
@@ -1,5 +1,17 @@
 package org.example;
 
-public class comparatorAndComparable {
-    
+import java.util.Comparator;
+
+public class NameAgeComparator implements Comparator<Employee> {
+
+
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        int flag = emp1.getSalary().compareTo(emp2.getSalary());
+        if(flag == 0) {
+            flag = emp1.getAge() - emp2.getAge();
+        }
+        return flag;
+    }
 }
+
